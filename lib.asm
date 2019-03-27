6af466d10a6bd7dd6ac4c09d85bf6a8f8627fee8
fran <fciarliero@dc.uba.ar>
2019-03-27 18:47:44 -0300

    agrega strClone

--- lib.asm
+++ lib.asm
@@ -32,10 +32,33 @@ global nTableRemoveSlot
 global nTableDeleteSlot
 global nTableDelete
 
-strLen:
+%define NULL 0
+
+;uint32_t strLen(char* a)
+strLen:                     ;rdi → a
+    push rbp
+    push rdi
+    push r12
+    mov rbp, rsp
+    sub rsp, 8
+    xor eax, eax            ;eax ← 0
+    .ciclo:
+        mov r12b, [rdi]     ;r12b ← [a] el char al que apunta el puntero a
+        cmp r12b, NULL      ;si este char es el char null, termine de recorrer el string
+        je .fin
+        inc eax             ;si no es null el char actual, incremento el contador y posiciono el puntero en el proximo char
+        inc rdi
+        jmp .ciclo
+    .fin:
+    add rsp, 8
+    pop r12
+    pop rdi
+    pop rbp
     ret
 
-strClone:
+;char* strClone(char* a)
+strClone:                   ;rdi ← a
+
     ret
 
 strCmp:
